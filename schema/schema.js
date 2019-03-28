52c9a38cd0150b1376cb09ecbec8defc2fcff0bc
Junjie Chen <junjie.chen18@gmail.com>
2019-03-27 22:54:34 -0400

    feat(schema): setup RootQueryType

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,8 @@ const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -12,4 +13,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
