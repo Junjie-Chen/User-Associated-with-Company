e983b863c45aa0c93c8b74a51b204135d5f84e75
Junjie Chen <junjie.chen18@gmail.com>
2019-03-31 14:06:14 -0400

    feat(schema): validate argument values and make the post request in the resolve method

--- schema/schema.js
+++ schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLSchema
 } = graphql;
 
@@ -68,17 +69,19 @@ const mutation = new GraphQLObjectType({
     addUser: {
       type: UserType,
       args: {
-        firstName: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
         companyId: { type: GraphQLString }
       },
-      resolve() {
-
+      resolve(parentValue, args) {
+        return axios.post('http://localhost:3000/users', args)
+          .then(res => res.data);
       }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
